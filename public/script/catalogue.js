bb632fa3bd09323585e0eaa1dfd4ca5f3c1d839f
Nicolas Sangsiri <nicolassangsiri@gmail.com>
2021-04-05 10:57:37 +0200

    prix panier custom maj dynamique

--- public/script/catalogue.js
+++ public/script/catalogue.js
@@ -13,6 +13,7 @@ $(document).ready(function()
     $("#collection").prop('disabled', true);
     $("#customMessage").hide();
     $("#orderCustom").hide();
+    $("#prixCustom").hide();
 });
 
 function createGallery(array){
@@ -43,7 +44,7 @@ function createImageGallery(array, i){
         '<div class="article">' +
             '<img class ="singleFlower" id="fleur'+i+'" src="../'+array[i].location + '" alt="fleur'+i+'">'+ 
             '<p class="item_name">'+array[i].name+'</p>'+
-            '<p class="item_price">'+(array[i].price).toFixed(2)+'€'+'</p>'+
+            '<p class="item_price"><span class="numericPrice">'+(array[i].price).toFixed(2)+'</span>€'+'</p>'+
             '<input type="number" class="quantitySelector" min="0" max="10" value="0"></input>'+
         '</div>'
         )
@@ -81,10 +82,12 @@ function swapGallery(){
     {
         $("#gallery").empty();
         if(showCustom == true){
+            $("#prixCustom").show();
             $("#customMessage").show();
             $("#orderCustom").show();
             $("#priceRange").hide();
         } else {
+            $("#prixCustom").hide();
             $("#customMessage").hide();
             $("#orderCustom").hide();
             $("#priceRange").show();
@@ -94,9 +97,18 @@ function swapGallery(){
 }
 
 function quantityChangeHandler(){
-    $('.quantitySelector').change(function()
+    $('#gallery').on('change', '.quantitySelector', function() 
     {
         $(this).attr('value', $(this).val());   
+        var customTotalPrice = 0.0;
+        $(".article").each(function(){
+            var item_price = parseFloat($(this).children('p.item_price').children("span.numericPrice").text());
+            var item_quantity = parseInt($(this).children(".quantitySelector").val());
+            if(item_quantity > 0){
+                customTotalPrice = customTotalPrice + (item_price*item_quantity);
+            } 
+        });
+        $("#realPrice").html(customTotalPrice.toFixed(2));
     });
 }
 
