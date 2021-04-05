a2428f437ef060fa14d048ca88ad7839d06460d9
Nicolas Sangsiri <nicolassangsiri@gmail.com>
2021-04-05 11:35:49 +0200

    oubli important incr et decr cart

--- public/script/cart.js
+++ public/script/cart.js
@@ -79,6 +79,8 @@ function decreaseBtnHandler(btn_clicked, article_id, article_quantity, index){
         $(btn_clicked).attr('onclick','decreaseBtnHandler(this,'+article_id+','+(article_quantity-1)+','+index+')');
         $(btn_clicked).siblings(".articleIncBtn").attr('onclick','increaseBtnHandler(this,'+article_id+','+(article_quantity-1)+','+index+')');
 
+        panier[index].quantite = (panier[index].quantite)-1;
+
         if(article_quantity-1 <= 1)//respect de l'invariant
         {
             $(btn_clicked).prop('disabled', true);
@@ -100,6 +102,8 @@ function increaseBtnHandler(btn_clicked, article_id, article_quantity, index){
 
         $(btn_clicked).attr('onclick','increaseBtnHandler(this' + ', '+article_id+','+(article_quantity+1)+','+index+')');
         $(btn_clicked).siblings(".articleDecBtn").attr('onclick','decreaseBtnHandler(this,'+article_id+','+(article_quantity+1)+','+index+')');
+
+        panier[index].quantite = (panier[index].quantite)+1;
 
         $(btn_clicked).siblings(".articleDecBtn").prop('disabled', false);
     });
